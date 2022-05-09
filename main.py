1b2608e6d78f61284bd4f89923955c1ff15c7b45
The0nes <93207468+The0nes@users.noreply.github.com>
2022-05-09 09:48:49 -0600

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pygame
 import random
 import math
 pygame.init()  
-pygame.display.set_caption("EEL")  # sets the window title
+pygame.display.set_caption("platformer")  # sets the window title
 screen = pygame.display.set_mode((1000, 1000))  # creates game screen
 screen.fill((0,0,0))
 clock = pygame.time.Clock() #set up clock
@@ -11,7 +11,10 @@ red = (255,0,0)
 
 
 Eel = pygame.image.load('eelface.png') #load your spritesheet
-Eel.set_colorkey((255,255,255)) #this makes bright pink (255, 0, 255) transparent (sort of)
+Eel.set_colorkey((255,255,255))
+Eel2 = pygame.image.load('eelface2.png') #load your spritesheet
+Eel2.set_colorkey((255,255,255))
+
 fishy = pygame.image.load('fishy.png')
 fishy.set_colorkey((255,255,255))
 Back = pygame.image.load('background.png')
@@ -21,16 +24,25 @@ LEFT=0
 RIGHT=1
 UP = 2
 DOWN = 3
-Px= 200 #xpos of player
-Py= 200 #ypos of player
+Px= 800 #xpos of player
+Py= 800 #ypos of player
+Px2 = 200
+Py2 = 200
+score = 0
+score2 = 0
 vx = 0 #x velocity of player
 vy = 0 #y velocity of player
+vx2 = 0
+vy2 = 0
 keys = [False, False, False, False] #this list holds whether each key has been pressed
+second = [False, False, False, False]
 
 frameWidth = 50
 frameHeight = 50
-RowNum = 0
+RowNum = 2
 frameNum = 0
+RowNum2 = 0
+frameNum2 = 0
 
 def CircleCollision(x1,x2,y1,y2, radius):
     if (math.sqrt((x2 - x1)**2 + (y2- y1)**2))<radius:
@@ -63,27 +75,46 @@ while not gameover: #GAME LOOP##################################################
             gameover = True
       
         if event.type == pygame.KEYDOWN: #keyboard input
-            if event.key == pygame.K_a:
+            if event.key == pygame.K_LEFT:
                 keys[LEFT]=True
-            elif event.key == pygame.K_d:
+            elif event.key == pygame.K_RIGHT:
                 keys[RIGHT]=True
-            elif event.key == pygame.K_w:
+            elif event.key == pygame.K_UP:
                 keys[UP]=True
-            elif event.key == pygame.K_s:
+            elif event.key == pygame.K_DOWN:
                 keys[DOWN]=True
+            if event.key == pygame.K_a:
+                second[LEFT]=True
+            elif event.key == pygame.K_d:
+                second[RIGHT]=True
+            elif event.key == pygame.K_w:
+                second[UP]=True
+            elif event.key == pygame.K_s:
+                second[DOWN]=True
            
         if event.type == pygame.KEYUP: #keyboard input
-            if event.key == pygame.K_a:
+            if event.key == pygame.K_LEFT:
                 keys[LEFT]=False
-            elif event.key == pygame.K_d:
+            elif event.key == pygame.K_RIGHT:
                 keys[RIGHT]=False
-            elif event.key == pygame.K_w:
+            elif event.key == pygame.K_UP:
                 keys[UP]=False
-            elif event.key == pygame.K_s:
+            elif event.key == pygame.K_DOWN:
                 keys[DOWN]=False
+            if event.key == pygame.K_a:
+                second[LEFT]=False
+            elif event.key == pygame.K_d:
+                second[RIGHT]=False
+            elif event.key == pygame.K_w:
+                second[UP]=False
+            elif event.key == pygame.K_s:
+                second[DOWN]=False
                 
-  
-    #physics/movement section#LEFT MOVEMENT
+    
+    
+    #physics section--------------------------------------------------------------------
+    
+    #LEFT MOVEMENT
     if keys[LEFT]==True:
         vx=-3
         vy=0
@@ -91,6 +122,7 @@ while not gameover: #GAME LOOP##################################################
         frameNum = 0
         direction = LEFT
         
+    #Right Movement
     elif keys[RIGHT]==True:
         vx=3
         vy=0
@@ -98,8 +130,6 @@ while not gameover: #GAME LOOP##################################################
         frameNum = 0
         direction = RIGHT
     
-
-        
       #JUMPING  
     if keys[UP]==True:
         vy=-3
@@ -107,20 +137,51 @@ while not gameover: #GAME LOOP##################################################
         RowNum = 1
         frameNum = 0
         direction = UP
-
         
       #DOWN
-    elif keys[DOWN]==True:
-        vy = 3
+    if keys[DOWN]==True:
+        vy=+3
         vx=0
         RowNum = 3
         frameNum = 0
         direction = DOWN
     
-
+    #LEFT MOVEMENT
+    if second[LEFT]==True:
+        vx2=-3
+        vy2=0
+        RowNum2 = 2
+        frameNum2 = 0
+        direction2 = LEFT
+        
+    #Right Movement
+    elif second[RIGHT]==True:
+        vx2=3
+        vy2=0
+        RowNum2 = 0
+        frameNum2 = 0
+        direction2 = RIGHT
+    
+      #JUMPING  
+    if second[UP]==True:
+        vy2=-3
+        vx2=0
+        RowNum2 = 1
+        frameNum2 = 0
+        direction2 = UP
+        
+      #DOWN
+    if second[DOWN]==True:
+        vy2=+3
+        vx2=0
+        RowNum2 = 3
+        frameNum2 = 0
+        direction2 = DOWN
     #update player position
     Px+=vx 
     Py+=vy
+    Px2+=vx2
+    Py2+=vy2
     
     #try to call the function here, use the new variables
     #(put the call inside an if statement, and only get new points for the circle when it's clicked on)
@@ -131,8 +192,19 @@ while not gameover: #GAME LOOP##################################################
         c2 = random.randrange(1, 255)
         c3 = random.randrange(1, 255)
         s = random.randrange(10, 21)
+        frameNum += 1
+        score += 1
         
-        
+    elif CircleCollision(num,Px2, Py2,num1, s)==True:
+        num = random.randrange(1, 800)
+        num1 = random.randrange(1, 800)
+        c1 = random.randrange(1, 255)
+        c2 = random.randrange(1, 255)
+        c3 = random.randrange(1, 255)
+        s = random.randrange(10, 21)
+        frameNum2 += 1
+        score2 += 1
+#player 1 warp zone
     if Px < 0:
         Px=999
     if Px > 999:
@@ -141,14 +213,27 @@ while not gameover: #GAME LOOP##################################################
         Py=999
     if Py > 999:
         Py=0
-   
+#player 2 warp zone        
+    if Px2 < 0:
+        Px2=999
+    if Px2 > 999:
+        Px2=0
+    if Py2 < 0:
+        Py2=999
+    if Py2 > 999:
+        Py2=0   
     #Render Section ---------------------------
     screen.fill((0,0,255))
     screen.blit(Back, (0,0), (0,0,1000,1000))
-
+    font = pygame.font.Font(None, 74)
+    text = font.render(str(score),1, (0, 255, 0))
+    screen.blit(text, (750, 10))
+    text = font.render(str(score2),1, (0, 255, 0))
+    screen.blit(text, (250, 10))
     screen.blit(fishy,(num, num1,25,25))
-    
-    screen.blit(Eel, (Px, Py), (frameWidth*frameNum, RowNum*frameHeight, frameWidth, frameHeight))    
+    screen.blit(Eel, (Px, Py), (frameWidth*frameNum, RowNum*frameHeight, frameWidth, frameHeight))
+    screen.blit(Eel2, (Px2, Py2), (frameWidth*frameNum2, RowNum2*frameHeight, frameWidth, frameHeight))    
+
 
     pygame.display.flip()
 
